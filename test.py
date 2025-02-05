2f25f01eccbeac2c3edf39f6c672656ff414df37
Celian <celian.daligault@gmail.com>
2025-02-05 16:05:22 +0100

    new

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ from pyspark.ml.regression import RandomForestRegressor
 from pyspark.ml.feature import VectorAssembler, MinMaxScaler
 import matplotlib.pyplot as plt
 
-spark = SparkSession.builder.appName("WeatherPrediction").getOrCreate()
-df = spark.read.csv("weather_data.csv", header=True, inferSchema=True)
+spark = SparkSession.builder.appName("WeatherPrediction").getOrCreate() #We create the spark session
+df = spark.read.csv("weather_data.csv", header=True, inferSchema=True) #We read the csv file
 df = df.withColumn("timestamp", to_timestamp(col("utc_timestamp"))).drop("utc_timestamp")
-df = df.dropna()
+df = df.dropna() 
+
+# We group the data by timestamp and calculate the mean and standard deviation of temperature and radiation
 
 df_grouped = df.groupBy("timestamp").agg(
     avg("FR_temperature").alias("mean_temp"),
@@ -16,53 +18,53 @@ df_grouped = df.groupBy("timestamp").agg(
     avg("FR_radiation_diffuse_horizontal").alias("mean_radiation_diffuse")
 )
 
+# assembler = VectorAssembler(inputCols=["mean_temp", "mean_radiation_direct", "mean_radiation_diffuse"], outputCol="features") #We create the vector assembler
+# df_ml = assembler.transform(df_grouped)
+# train, test = df_ml.randomSplit([0.8, 0.2], seed=42)
+# model = RandomForestRegressor(featuresCol="features", labelCol="mean_temp", numTrees=10)
+# trained_model = model.fit(train)
+# predictions = trained_model.transform(test)
+# predictions_pd = predictions.select("timestamp", "mean_temp", "prediction").toPandas()
 
-assembler = VectorAssembler(inputCols=["mean_temp", "mean_radiation_direct", "mean_radiation_diffuse"], outputCol="features")
-df_ml = assembler.transform(df_grouped)
-train, test = df_ml.randomSplit([0.8, 0.2], seed=42)
-
-model = RandomForestRegressor(featuresCol="features", labelCol="mean_temp", numTrees=10)
-trained_model = model.fit(train)
-predictions = trained_model.transform(test)
 df_pandas = df_grouped.toPandas()
-predictions_pd = predictions.select("timestamp", "mean_temp", "prediction").toPandas()
+
+# plt.figure(figsize=(10, 5))
+# plt.plot(predictions_pd["timestamp"], predictions_pd["mean_temp"], label="Real Mean Temperature", color='b')
+# plt.plot(predictions_pd["timestamp"], predictions_pd["prediction"], label="Predicted Mean Temperature", color='r', linestyle='--')
+# plt.xlabel("Date")
+# plt.ylabel("Mean Temperature (°C)")
+# plt.legend()
+# plt.grid()
+# plt.savefig("temperature_predictions_plot.png")
+# # plt.show()
+
 
 plt.figure(figsize=(10, 5))
-plt.plot(predictions_pd["timestamp"], predictions_pd["mean_temp"], label="Real Mean Temperature", color='b')
-plt.plot(predictions_pd["timestamp"], predictions_pd["prediction"], label="Predicted Mean Temperature", color='r', linestyle='--')
-plt.xlabel("Date")
-plt.ylabel("Mean Temperature (°C)")
-plt.legend()
-plt.grid()
-plt.savefig("temperature_predictions_plot.png")
-# plt.show()
-
-plt.figure(figsize=(10, 5))
-plt.plot(df_pandas["timestamp"], df_pandas["mean_temp"], label="Mean Temperature FR", color='b')
+plt.scatter(df_pandas["timestamp"], df_pandas["mean_temp"], label="Mean Temperature FR", color='b', s=5)
 plt.xlabel("Date")
 plt.ylabel("Mean Temperature (°C)")
 plt.legend()
 plt.grid()
 plt.savefig("temperature_plot.png")
-# plt.show()
+plt.show()
 
 plt.figure(figsize=(10, 5))
-plt.plot(df_pandas["timestamp"], df_pandas["mean_radiation_direct"], label="Mean Direct Radiation", color='r')
+plt.scatter(df_pandas["timestamp"], df_pandas["mean_radiation_direct"], label="Mean Direct Radiation", color='r', s=5)
 plt.xlabel("Date")
 plt.ylabel("Mean Direct Radiation")
 plt.legend()
 plt.grid()
 plt.savefig("radiation_direct_plot.png")
-# plt.show()
+plt.show()
 
 plt.figure(figsize=(10, 5))
-plt.plot(df_pandas["timestamp"], df_pandas["mean_radiation_diffuse"], label="Mean Diffuse Radiation", color='g')
+plt.scatter(df_pandas["timestamp"], df_pandas["mean_radiation_diffuse"], label="Mean Diffuse Radiation", color='g', s=5)
 plt.xlabel("Date")
 plt.ylabel("Mean Diffuse Radiation")
 plt.legend()
 plt.grid()
 plt.savefig("radiation_diffuse_plot.png")
-# plt.show()
+plt.show()
 
 with open("README.md", "w") as f:
     f.write("# Weather Prediction Report\n\n")
@@ -76,5 +78,5 @@ with open("README.md", "w") as f:
     f.write("### Mean Diffuse Radiation Trends\n")
     f.write("![Diffuse Radiation Trends](radiation_diffuse_plot.png)\n\n")
     f.write("### Model Performance\n")
-    predictions_str = predictions.select("mean_temp", "prediction").toPandas().to_string(index=False)
-    f.write(predictions_str)
+#     predictions_str = predictions.select("mean_temp", "prediction").toPandas().to_string(index=False)
+#     f.write(predictions_str)
